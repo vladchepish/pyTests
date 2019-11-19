74de60a406813dc513685a9212c8f21746ebddce
Vlad <vladforwork@yandex.ru>
2019-11-19 16:51:23 +0300

    added primitive dataGenerator

--- test/group/test_add_group.py
+++ test/group/test_add_group.py
@@ -1,21 +1,22 @@
 from model.group import Group
+import pytest
+from utils.dataGenerator import random_string
 
 
-def test_add_group(app):
+testdata = [
+    Group(name=random_string("name", 10), header=random_string("header", 20), footer=random_string("footer", 15)),
+    Group(name=random_string("name", 10), header=random_string("header", 20), footer=random_string("footer", 15)),
+    Group(name=random_string("name", 10), header=random_string("header", 20), footer=random_string("footer", 15)),
+    Group(name=random_string("name", 10), header=random_string("header", 20), footer=random_string("footer", 15)),
+    Group(name=random_string("name", 10), header=random_string("header", 20), footer=random_string("footer", 15)),
+]
+
+
+@pytest.mark.parametrize("group", testdata, ids=[repr(x) for x in testdata])
+def test_add_group(app, group):
     old_groups = app.group.get_group_list()
-    added_group = Group(name="pyTestsGroup", header="header", footer="footer")
-    app.group.create(added_group)
+    app.group.create(group)
     new_groups = app.group.get_group_list()
     assert len(old_groups) + 1 == len(new_groups)
-    old_groups.append(added_group)
-    assert sorted(old_groups, key=Group.sor_by_id) == sorted(new_groups, key=Group.sor_by_id)
-
-
-def test_add_empty_group(app):
-    old_groups = app.group.get_group_list()
-    added_group = Group(name="", header="", footer="")
-    app.group.create(added_group)
-    new_groups = app.group.get_group_list()
-    assert len(old_groups) + 1 == len(new_groups)
-    old_groups.append(added_group)
+    old_groups.append(group)
     assert sorted(old_groups, key=Group.sor_by_id) == sorted(new_groups, key=Group.sor_by_id)
